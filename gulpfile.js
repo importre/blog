6513d2d870bade9eb58089e14ddded7c88595e70
Jaewe Heo <jaeweheo@gmail.com>
2015-06-05 00:28:44 +0900

    update deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var del = require('delete');
+var shell = require('gulp-shell')
 
 gulp.task('deploy', function () {
   var opt = {
     branch: "master"
   };
   del.sync('.publish');
+  shell.task([
+    'rm -rf public',
+    'hugo'
+  ])
   return gulp.src('./public/**/*').pipe(ghPages(opt));
 });
