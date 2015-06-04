05f1b1d1fb7dca6cdc5647faa5daa38d0a57e80d
Jaewe Heo <jaeweheo@gmail.com>
2015-06-04 22:20:33 +0900

    add delete module

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var del = require('delete');
 
-gulp.task('deploy', function() {
+gulp.task('deploy', function () {
   var opt = {
     branch: "master"
   };
+  del.sync('.publish');
   return gulp.src('./public/**/*').pipe(ghPages(opt));
 });
