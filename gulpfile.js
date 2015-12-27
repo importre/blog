d274dbba81ce732b7e6ea4fa8822d0d1985696c1
Jaewe Heo <jaeweheo@gmail.com>
2015-12-27 16:28:35 +0900

    add static files

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,37 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var del = require('delete');
 var shell = require('gulp-shell');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('clean', shell.task([
   'rm -rf public'
 ]));
 
-gulp.task('build', ['clean'], shell.task([
+gulp.task('dep', ['clean'], function () {
+  del.sync('static/js');
+  del.sync('static/css');
+  del.sync('static/fonts');
+  gulp.src('node_modules/jquery/dist/*')
+      .pipe(gulp.dest('static/js'));
+  gulp.src('node_modules/bootstrap/dist/css/*')
+      .pipe(gulp.dest('static/css'));
+  gulp.src('node_modules/bootstrap/dist/fonts/*')
+      .pipe(gulp.dest('static/fonts'));
+  gulp.src('node_modules/bootstrap/dist/js/*')
+      .pipe(gulp.dest('static/js'));
+  gulp.src('node_modules/highlight.js/styles/github.css')
+      .pipe(gulp.dest('static/css'));
+  gulp.src('node_modules/font-awesome/css/*')
+      .pipe(gulp.dest('static/css'));
+  gulp.src('node_modules/font-awesome/fonts/*')
+      .pipe(gulp.dest('static/fonts'));
+  browserify('js/highlight.js').bundle()
+      .pipe(source('highlight.pack.js'))
+      .pipe(gulp.dest('static/js'));
+});
+
+gulp.task('build', ['dep'], shell.task([
   'hugo'
 ]));
 
@@ -16,6 +41,6 @@ gulp.task('deploy', ['build'], function () {
   return gulp.src('./public/**/*').pipe(ghPages());
 });
 
-gulp.task('watch', ['clean'], shell.task([
+gulp.task('watch', ['dep'], shell.task([
   'hugo server --watch --verbose'
 ]));
